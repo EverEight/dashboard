c718673cf4e1851db0d1a0f51d0a307fa6138f94
Hachi-R <drive8hachi@gmail.com>
2024-06-01 15:43:12 -0300

    add geist font sans and mono

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,10 @@
   // Theme Mode
   import { ModeWatcher } from "mode-watcher";
 </script>
+<svelte:head> 
+  <link rel="preload" as="font" href="/fonts/Geist/GeistVF.woff2" type="font/woff2" crossorigin="anonymous"> 
+  <link rel="preload" as="font" href="/fonts/Geist/GeistMonoVF.woff2" type="font/woff2" crossorigin="anonymous"> 
+</svelte:head>
 
 <ModeWatcher />
 <slot />
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -55,7 +55,8 @@ const config: Config = {
         sm: "calc(var(--radius) - 4px)",
       },
       fontFamily: {
-        sans: [...fontFamily.sans],
+        sans: ["geist-sans", ...fontFamily.sans],
+        mono: ["geist-mono", ...fontFamily.mono],
       },
     },
   },
